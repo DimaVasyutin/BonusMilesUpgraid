29e67c69b6b5ed1cdb806732527503950cf2f555
DimaVasyutin <115107603+DimaVasyutin@users.noreply.github.com>
2022-11-05 13:24:52 +0300

    Reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,8 @@
 public class BonusMilesService {
-    public int calculate(int cost){
-        byte oneMileCost =20;
+    public int calculate(int cost) {
+        byte oneMileCost = 20;
         int sumMiles = cost / oneMileCost;
         return sumMiles;
     }
 
 }
-
